b7e1611785436a2a620314f579a679e923c6a8c6
Mitra Nami <a.mxnami@gmail.com>
2021-01-28 17:46:18 -0500

    Use async/await to fetch and display the files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // Using promises
 
 function fetchAndDecode(url, type) {
@@ -45,4 +46,58 @@ Promise.all([tea, coffee, test])
 
   })
   .catch(e => console.log(e));
+
+
+*/
+
+
+
+// Using async/await which returns a promise resolved with content
+// exactly like when we used promises
+
+async function fetchAndDecode(url, type) {
+
+  let content;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  if (type === 'blob') {
+    content = await response.blob(); // contecnt is the blob
+  } else if (type === 'text') {
+    content = await response.text(); // content is the text
+  }
+  return content; //since the function has an async keyword on it, it will return a promise which is resolved with content 
+  
+}
+
+// it's similar to what we did with then but since we want to use await for Promise.all we need to put the code block in an async function
+async function displayContent () {
+
+  const tea = fetchAndDecode('./images/tea.jpg', 'blob');
+  const coffee = fetchAndDecode('./images/coffee.jpg', 'blob');
+  const test = fetchAndDecode('./test.txt', 'text');
+
+  const [teaBlob, coffeeBlob, testText] =
+    await Promise.all([tea, coffee, test]);
+
+    const objectURLTea = URL.createObjectURL(teaBlob);
+    const teaPic = document.createElement('img');
+    teaPic.src = objectURLTea;
+    document.body.appendChild(teaPic);
+
+    const objectURLCoffee = URL.createObjectURL(coffeeBlob);
+    const coffeePic = document.createElement('img');
+    coffeePic.src = objectURLCoffee;
+    document.body.appendChild(coffeePic);
+
+    const par = document.createElement('p');
+    par.textContent = testText;
+    document.body.appendChild(par);
+
+}
+
+displayContent()
+  .catch(e => console.log(e))
+
 
